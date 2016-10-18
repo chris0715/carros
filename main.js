053a5810336caae2e8eebddd5a8481b5094ef78a
muldog <christopher.encarnacion@gmail.com>
2016-10-18 07:47:51 -0400

    this is sparta

--- main.js
+++ main.js
@@ -25,6 +25,6 @@ app.get('/', function (req,res){
 
  var portToUse = process.env.PORT;
 
-app.listen(8080, "0.0.0.0", function(){
+app.listen(process.env.PORT, "0.0.0.0", function(){
     console.log('Listening on port '+this.address().port);
 });
